95c97e6fc362474504def8651f47ab2475112216
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-14 11:50:01 +0600

    Update TJP9.c

--- TJP9.c
+++ TJP9.c
@@ -1,3 +1,5 @@
+// P-9. Write a C program which can input running time of a movie by inputting two numbers mentioning hour and minute of that movie. Display what is the running time of that movie in minutes. Display running time in fractional hour format. As example, if input is 1 hour 15 minutes, then one output is 75 minutes and another output is 1.25 hours. //
+
 #include <stdio.h>
 int main(){
 int hours, minutes, totalMinutes;
