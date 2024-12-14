1e1569142882964d6a0e7bc299b1a2327081d73b
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-14 11:50:40 +0600

    Update TJP10.c

--- TJP10.c
+++ TJP10.c
@@ -1,3 +1,5 @@
+// P-10. Write a C program which can input 4 integer numbers from the users and display the ratio between differences of first two numbers and last two numbers. As example, if input values are A, B, C, and D then find (A-B)/(C-D). //
+
 #include <stdio.h>
 int main(){
 int A, B, C, D;
